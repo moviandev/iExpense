9fbebeae8fce70f078e5b1b4e19bc66dfa44e062
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-09 20:41:19 -0300

    Adding a sheet view and learn how to pass custom properties to it

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,10 +7,24 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SecondView: View {
+    let name: String
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello \(name)")
+    }
+}
+
+struct ContentView: View {
+    @State private var showingSheet = false
+    
+    var body: some View {
+        Button("Show Sheet") {
+            showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "Movian")
+        }
     }
 }
 
