eb31f09e5266b288a3dc7dc54af70274f8ff0d41
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-09 20:51:26 -0300

    Add on delete, to perform deletion of rows in our list, to it work we need to use IndexSet which tells exactly what index should be  removed

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List {
+                ForEach(numbers, id: \.self) {
+                    Text("Row \($0)")
+                }
+                .onDelete(perform: removeRows)
+            }
+            
+            Button("Add number") {
+                numbers.append(currentNumber)
+                currentNumber += 1
+            }
+        }
     }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
