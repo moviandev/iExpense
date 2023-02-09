c9d980704d1611bd3cebf831bf851a5bbd50f6cd
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-09 20:53:36 -0300

    Rollback

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,35 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
-    
     var body: some View {
-        NavigationView {
-            VStack {
-                List {
-                    ForEach(numbers, id: \.self) {
-                        Text("Row \($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-                
-                Button("Add number") {
-                    numbers.append(currentNumber)
-                    currentNumber += 1
-                }
-            }
-            .navigationTitle("onDelete()")
-            .toolbar {
-                EditButton()
-            }
-        }
+        Text("Hello, world!")
+            .padding()
     }
-    
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
-    }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
