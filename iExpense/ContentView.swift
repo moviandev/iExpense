4a2989ba6992e77b53fe4f7ffe2596b5dc83298d
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-09 20:21:54 -0300

    create struct user and set default properties

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,21 @@
 
 import SwiftUI
 
+struct User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
 struct ContentView: View {
+    @State private var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("First name", text: $user.lastName)
         }
-        .padding()
     }
 }
 
