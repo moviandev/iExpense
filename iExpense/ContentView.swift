1fe95e61abe58de8d6ffd005f21e22e4e8c93327
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-09 20:35:21 -0300

    Rollback

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,21 +7,10 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
-}
-
 struct ContentView: View {
-    @StateObject var user = User()
-    
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("First name", text: $user.lastName)
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
