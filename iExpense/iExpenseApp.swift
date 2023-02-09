19a2be7d4b25e7a7abe1d1ff8274fe5013e83221
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-09 20:17:15 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Matheus Viana on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
