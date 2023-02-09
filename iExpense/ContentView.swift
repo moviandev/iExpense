79466dbb9632d2cb792e9e7e6c965976ade89ac6
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-09 20:44:48 -0300

    Rollback

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,28 +7,10 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
-    
-    var body: some View {
-        Button("Dismiss") {
-            dismiss()
-        }
-    }
-}
-
 struct ContentView: View {
-    @State private var showingSheet = false
-    
     var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Movian")
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
