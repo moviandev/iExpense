f5c07b8642e678e1260c0e8ec91abeb05cc47a34
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-09 21:21:47 -0300

    Rollback

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,22 +7,10 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let firstName: String
-    let lastName: String
-}
-
 struct ContentView: View {
-    @State private var user = User(firstName: "Taylor", lastName: "Swift")
-    
     var body: some View {
-        Button("Save user") {
-            let encoder = JSONEncoder()
-            
-            if let data = try? encoder.encode(user) {
-                UserDefaults.standard.set(data, forKey: "UserData")
-            }
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
