43350ce1baee08a43d0391d3947738de4d326410
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-10 15:35:37 -0300

    Saving data in our array

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -31,6 +31,12 @@ struct AddView: View {
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add New Expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, tyoe: type, amount: amount)
+                    expenses.items.append(item)
+                }
+            }
         }
     }
 }
