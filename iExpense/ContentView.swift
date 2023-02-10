3e33410d3fa64bc3c91601d097d58b47b8d46606
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-09 21:21:22 -0300

    How to use codable to save "complex" data in UserDefaults

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,10 +7,22 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
+
 struct ContentView: View {
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Save user") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
     }
 }
 
