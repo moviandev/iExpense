ed926891f9ab83e3790441f9da7ce2a9a1ed770c
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-09 21:10:31 -0300

    Using UserDefaults to store data of the user in our app

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap count \(tapCount)") {
+            tapCount += 1
+            UserDefaults.standard.set(tapCount, forKey: "Tap")
+        }
     }
 }
 
