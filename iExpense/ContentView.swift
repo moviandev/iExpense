c37d66892a884ee3c3c4a2b7778f356980fcf76c
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-10 15:06:35 -0300

    Adding items in our expense list

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    let expense = ExpenseItem(name: "Test", tyoe: "Personal", amount: 0)
+                    expenses.items.append(expense)
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
     }
 }
 
